7f2df9f5c84b89f2a8d5f899c5812fb59e2c2823
Yamita0011 <a.diaz.m9292@edu.utc.mx>
2024-01-18 21:22:45 -0600

    Mi primera chamba

---
+++ 01PrimeraChamba/HolaMundo.java
@@ -0,0 +1,29 @@
+// vamos a crear un primer porgrama
+
+/*Para poder comentar en distintas lineas de codigo */
+
+public class HolaMundo{
+ 
+    
+    //los metodos son los comportamientos que va a tener una clase
+    //el comportamiento son loas acciones u operaciones para las que
+    //estan distinadas las clases 
+
+    //Existen metodos principales (main)
+    //Metodos getter and setter intercambio de datos
+    //Metodos constructores los contructores son los encargados
+    //de la estructura de la clase, inicializar las varibales,
+    //crear las instancias 
+    //Metodos por tipo de dato no preferiamos a int, float, String, etc
+
+    //los obajetos son instancia (invociones) la escructrura abtracta
+    //de la cosa
+
+    //las caracteristicas del objeto son los atributos de la clase
+
+    public static void main(String[] args) {
+        System.out.print("Si sale error date de baja de la vida");
+
+    }
+
+}
